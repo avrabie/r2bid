15c03d796e63682b9dd0e3d09a7c8a075e264b13
moldo <net.vrabie@gmail.com>
2025-03-11 00:23:11 +0100

    reficing the pojo

--- src/main/java/com/execodex/r2bid/service/StockPriceResponse.java
+++ src/main/java/com/execodex/r2bid/service/StockPriceResponse.java
@@ -17,11 +17,18 @@ public class StockPriceResponse {
 
 @Data
 class MetaData {
+    @JsonProperty("1. Information")
+    private String information;
     @JsonProperty("2. Symbol")
     private String symbol;
-
     @JsonProperty("3. Last Refreshed")
     private String lastRefreshed;
+    @JsonProperty("4. Interval")
+    private String interval;
+    @JsonProperty("5. Output Size")
+    private String outputSize;
+    @JsonProperty("6. Time Zone")
+    private String timezone;
 }
 
 @Data
